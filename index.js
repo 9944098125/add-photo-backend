90dfe7deb540fba69022703ca33dd7938b0c67e6
Srinivas Akella <srinivas.prasad@teknotrait.com>
2023-07-13 12:47:52 +0530

    updated code with taking image from frontend normally but converting that to binary and then base 64 string and fetching small images

--- index.js
+++ index.js
@@ -1,47 +1,69 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const multer = require("multer");
+const mySql = require("mysql2/promise");
+const fs = require("fs");
 const cors = require("cors");
-const mySql = require("mysql2");
 
 const app = express();
+const upload = multer({ dest: "uploads/" });
 
-const db = mySql.createPool({
+const pool = mySql.createPool({
   host: "localhost",
   user: "root",
   password: "Srinivas@8",
   database: "add_photo",
 });
 
-app.use(express.json());
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("/api/post-users", (req, res) => {
-  const { name, email, photo } = req.body;
-  const sql = "INSERT INTO users (name, email, photo) VALUES (?, ?, ?)";
-  db.query(sql, [name, email, photo], (err, result) => {
-    if (err) {
-      console.log(err);
-      return res.status(401).json({ message: "Something went wrong" });
-    } else if (result.affectedRows > 0) {
-      // console.log(result);
-      res.status(201).json({ message: "User created successfully" });
-    }
-  });
+// Handle image upload
+app.post("/upload", upload.single("image"), async (req, res) => {
+  const { name, email } = req.body;
+  const imagePath = req.file.path;
+
+  try {
+    // Read the image file as binary data
+    const imageBuffer = await fs.promises.readFile(imagePath);
+
+    // Insert the data into the database
+    const insertQuery =
+      "INSERT INTO users (name, email, image) VALUES (?, ?, ?)";
+    await pool.query(insertQuery, [name, email, imageBuffer]);
+
+    res.status(200).json({ message: "Image uploaded successfully!" });
+  } catch (error) {
+    console.error("Error uploading image:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
-app.get("/api/get-users", (req, res) => {
-  const sql = "SELECT * FROM users";
-  db.query(sql, (err, result) => {
-    // console.log(result);
-    if (err) {
-      return res.status(400).json({ message: "Something went wrong" });
-    } else {
-      res
-        .status(200)
-        .json({ message: "Users fetched successfully", users: result });
-    }
-  });
+// Retrieve user data with images
+app.get("/users", async (req, res) => {
+  try {
+    const selectQuery = "SELECT id, name, email, image FROM users";
+    const [rows] = await pool.query(selectQuery);
+
+    // Convert image data to Base64-encoded string
+    const users = await Promise.all(
+      rows.map(async (row) => {
+        const user = { ...row };
+        console.log(row);
+
+        if (row) {
+          user.image = row.image.toString("base64");
+        }
+        // console.log(user);
+        return user;
+      })
+    );
+
+    res.status(200).json(users);
+  } catch (error) {
+    console.error("Error retrieving users:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
-app.listen(4000, () => console.log(`App is now running on port 4000`));
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
